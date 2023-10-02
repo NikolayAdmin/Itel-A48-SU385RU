327b0531cbf4cceee958cd5263b1356c1fd8de9a
GhostViper2007 <140882754+GhostViper2007@users.noreply.github.com>
2023-10-02 17:27:23 +0300

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+
+#add_lunch_combo twrp_L6006-userdebug
+#add_lunch_combo twrp_L6006-eng
