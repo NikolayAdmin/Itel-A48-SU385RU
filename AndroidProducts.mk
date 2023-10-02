528ccb6928d5ab86b832ec1dce2471a368aa1682
GhostViper2007 <140882754+GhostViper2007@users.noreply.github.com>
2023-10-02 16:37:48 +0300

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/twrp_L6006.mk
+
+     
+COMMON_LUNCH_CHOICES := \
+    twrp_L6006-eng \
+    twrp_L6006-userdebug \
+    twrp_L6006-user
