472c4fc66a857160691e794a7fad6e3064149b84
GhostViper2007 <140882754+GhostViper2007@users.noreply.github.com>
2023-10-02 16:36:09 +0300

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE), L6006)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
