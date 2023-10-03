19e5f5332c18f6812379715dff5e510b9c04a76b
GhostViper2007 <140882754+GhostViper2007@users.noreply.github.com>
2023-10-03 22:36:57 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,11 +3,8 @@
 # Copyright (C) 2023 The TWRP Open Source Project
 #
 
-PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_L6006.mk
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_DIR)/init
 
-     
-COMMON_LUNCH_CHOICES := \
-    twrp_L6006-eng \
-    twrp_L6006-userdebug \
-    twrp_L6006-user
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_L6006.mk
+
+COMMON_LUNCH_CHOICES := twrp_L6006-eng
