fe3a32ae42faf0c76f3bec4d9d3b677b1e3dd289
GhostViper2007 <140882754+GhostViper2007@users.noreply.github.com>
2023-10-03 22:42:32 +0300

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-#
-
-#add_lunch_combo twrp_L6006-userdebug
-#add_lunch_combo twrp_L6006-eng
