d98e8bf706856c4157b2b261dc013747428b6786
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-03-25 14:55:09 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-#
-
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_DIR)/init
-
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_L6006.mk
-
-COMMON_LUNCH_CHOICES := twrp_L6006-eng
