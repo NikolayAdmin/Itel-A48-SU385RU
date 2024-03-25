031614b076d24c7c881a2f2edf0ed3edc605c6be
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-03-25 21:43:42 +0300

    Update twrp_L6006.mk

--- twrp_L6006.mk
+++ twrp_L6006.mk
@@ -1,17 +1,15 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-#
+# Inherit from L6006 device
+$(call inherit-product, device/itel/L6006/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-# Inherit from L6006 device
-$(call inherit-product, device/itel/L6006/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := L6006
@@ -20,12 +18,3 @@ PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel L6006
 PRODUCT_MANUFACTURER := ITEL MOBILE LIMITED
 PRODUCT_RELEASE_NAME := L6006
-
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="L6006-SU385-10-RU-V021-20230214"
-
-##    TARGET_DEVICE=L6006 \
-##    PRODUCT_NAME=L6006 \
-
-BUILD_FINGERPRINT := Itel/SU385RU/itel-L6006:10/QP1A.190711.020/RU-V021-20230214:user/release-keys
