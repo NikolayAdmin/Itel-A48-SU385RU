0c19dbdebfc9d29923db2b21e8466f517600e73c
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-03-31 18:31:27 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,20 +2,13 @@
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
 ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # AVB
@@ -173,7 +166,7 @@ TW_USE_TOOLBOX := true
 TARGET_USES_MKE2FS := true
 
 # Vendor modules loading
-TW_LOAD_VENDOR_MODULES := "tlsc6x_ts.ko"
+TW_LOAD_VENDOR_MODULES := "mali.ko sprd_sensor.ko sunwave_sw9058g_fp.ko tlsc6x_ts.ko"
 
 # Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := vendor
