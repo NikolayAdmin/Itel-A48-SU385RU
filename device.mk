2ed533ac11f78328f2fb4ed5898c4ce90f35c64a
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-03-31 19:52:50 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,8 +18,8 @@ PRODUCT_PACKAGES += \
 
 # Health Hal
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Platform
 PRODUCT_PLATFORM := sp9832e
