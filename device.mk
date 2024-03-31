95684c1cd4a05e514fd94b8cbfe506d738747e0f
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-03-31 19:48:58 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,6 +10,17 @@ LOCAL_PATH := device/itel/L6006
 # Dynamic Partitions stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Platform
 PRODUCT_PLATFORM := sp9832e
 
