48b1dc7dd5e81a40044707ce14d1e22f404825a9
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-04-01 07:16:30 +0300

    Update twrp_L6006.mk

--- twrp_L6006.mk
+++ twrp_L6006.mk
@@ -1,14 +1,13 @@
 # Inherit from L6006 device
 $(call inherit-product, device/itel/L6006/device.mk)
 
-# Inherit from those products. Most specific first.
+# Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+# Inherit some common TWRP stuff
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device identifier. This must come after all inclusions
