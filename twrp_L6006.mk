b9d2135eff0e433fd053086173ae3fd37261e810
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-10-29 11:09:19 +0300

    Update twrp_L6006.mk

--- twrp_L6006.mk
+++ twrp_L6006.mk
@@ -1,5 +1,5 @@
 # Inherit from L6006 device
-$(call inherit-product, device/itel/L6006/device.mk)
+$(call inherit-product, device/Itel/SU385RU/device.mk)
 
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -11,9 +11,9 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := L6006
-PRODUCT_NAME := twrp_L6006
+PRODUCT_DEVICE := SU385RU
+PRODUCT_NAME := SU385RU
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel L6006
 PRODUCT_MANUFACTURER := ITEL MOBILE LIMITED
-PRODUCT_RELEASE_NAME := L6006
+PRODUCT_RELEASE_NAME := SU385RU
