b7f28df646a7fdebf0374e044ad08a0515dcf965
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-10-30 11:57:03 +0300

    Update twrp_itel-L6006.mk

--- twrp_itel-L6006.mk
+++ twrp_itel-L6006.mk
@@ -12,8 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := SU385RU
-PRODUCT_NAME := SU385RU
+PRODUCT_NAME := twrp_SU385RU
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel L6006
 PRODUCT_MANUFACTURER := ITEL MOBILE LIMITED
-PRODUCT_RELEASE_NAME := SU385RU
