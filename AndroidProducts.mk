8aceddd30cf69fbd6660e129fe5b1c5557a436f7
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-12-16 01:25:55 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,4 @@
-COMMON_LUNCH_CHOICES := twrp_itel-L6006-userdebug
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_itel-L6006.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_itel-L6006.mk
+
+COMMON_LUNCH_CHOICES := omni_itel-L6006-eng
