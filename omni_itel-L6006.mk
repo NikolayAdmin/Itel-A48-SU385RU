9e3b4fffa4fcd3d45dadc81b45ae721c8b42f9f2
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-12-16 01:23:04 +0300

    Update and rename twrp_itel-L6006.mk to omni_itel-L6006.mk

--- omni_itel-L6006.mk
+++ omni_itel-L6006.mk
@@ -4,8 +4,8 @@ $(call inherit-product, device/Itel/itel-L6006/device.mk)
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common TWRP stuff
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from our PitchBlack configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
