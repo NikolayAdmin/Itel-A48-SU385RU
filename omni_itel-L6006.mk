2b6cf2422bd52d581e57986e3e26013e860f6270
NikolayAdmin <153384004+NikolayAdmin@users.noreply.github.com>
2024-12-16 01:49:28 +0300

    Update omni_itel-L6006.mk

--- omni_itel-L6006.mk
+++ omni_itel-L6006.mk
@@ -1,3 +1,6 @@
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from itel-L6006 device
 $(call inherit-product, device/Itel/itel-L6006/device.mk)
 
@@ -7,12 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from our PitchBlack configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := itel-L6006
 PRODUCT_NAME := omni_itel-L6006
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel L6006
 PRODUCT_MANUFACTURER := ITEL MOBILE LIMITED
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
